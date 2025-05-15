57eebe95224467bf6a2da815bc798cfdd3cf7d7b
Дима <89656340841@mail.ru>
2025-05-15 20:07:55 +0300

    Расширен алфавит для генерации пароля: добавлены цифры и специальные символы

--- password_generator.py
+++ password_generator.py
@@ -2,7 +2,7 @@ import random
 import string
 
 def generate_password(length):
-    alphabet = string.ascii_lowercase
+    alphabet = string.ascii_letters + string.digits + string.punctuation  # Добавлены цифры и специальные символы
     password = .join(random.choice(alphabet) for _ in range(length))
     return password
 
