2fa79c807cea3546c3fc7e77c84e63d6ca3a4c52
Дима <89656340841@mail.ru>
2025-05-15 20:05:02 +0300

    Расширен алфавит для генерации пароля

--- password_generator.py
+++ password_generator.py
@@ -2,7 +2,7 @@ import random
 import string
 
 def generate_password(length):
-    alphabet = string.ascii_lowercase
+    alphabet = string.ascii_letters  # Добавлен верхний регистр
     password = .join(random.choice(alphabet) for _ in range(length))
     return password
 
