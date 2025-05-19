bdcf19f6cc95f8a794c42bfd1264d9f3690b45cf
Дима <89656340841@mail.ru>
2025-05-19 18:48:40 +0300

    Обнавленный файл password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -2,10 +2,8 @@ import random
 import string
 
 def generate_password(length):
-<<<<<<< HEAD
     alphabet = string.ascii_letters + string.digits + string.punctuation  # Добавлены цифры и специальные символы
->>>>>>> dev
-    password = .join(random.choice(alphabet) for _ in range(length))
+    password = ''.join(random.choice(alphabet) for _ in range(length))  # Исправлено на ''
     return password
 
 print(generate_password(10))
